402613f4bbd2cebf58e19648e47cf085fe727ee3
late4marshmellow <79985161+late4marshmellow@users.noreply.github.com>
2022-07-01 17:07:07 +0200

    Update create_proxy.sh

--- create_proxy.sh
+++ create_proxy.sh
@@ -2,32 +2,43 @@
 read -p "Fresh Installation? y/n: " answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then 
     echo Yes
+    
+    echo "[1] - Installing updates"
+    apt update && sudo apt upgrade
+
+    echo "[2] - Setting up a proxy server"
+    yes | sudo apt install dante-server
+    yes | sudo apt install curl
+
 else
-    echo No
-fi
+echo No
+
 read -p "Press 'Enter' to exit"
 exit
 
-echo "[1] - Installing updates"
-apt update && sudo apt upgrade
-
-echo "[2] - Setting up a proxy server"
-yes | sudo apt install dante-server
-yes | sudo apt install curl
-
 echo "[3] - Getting a network profile"
+read -p "adress to ping?: " pingad
+read -p "$pingad - is this correct? y/n" answer2
+if [ "$answer2" != "${answer2#[Yy]}" ] ;then 
 ext_interface () {
     for interface in /sys/class/net/*
     do
         [[ "${interface##*/}" != 'lo' ]] && \
-            ping -c1 -W2 -I "${interface##*/}" speedtest.net >/dev/null 2>&1 && \
+            ping -c1 -W2 -I "${interface##*/}" ${pingad} >/dev/null 2>&1 && \
                 printf '%s' "${interface##*/}" && return 0
     done
 }
 
+else
+exit
+fi
+
 echo "[4] - Adding a config server"
 echo "logoutput: stderr" > /etc/danted.conf
-for ((i = 7000; i <= 7010; i++))
+echo"Define portrange"
+read -p "First in port range: " port1
+read -p "Last in port range: " port2
+for ((i = ${port1}; i <= ${port2}; i++))
     do
         echo "internal: $(ext_interface) port = $i" >> /etc/danted.conf
         echo "[5] - Adding rules to the server"
@@ -48,9 +59,9 @@ echo "        from: 0.0.0.0/0 to: 0.0.0.0/0" >> /etc/danted.conf
 echo "        log: connect error" >> /etc/danted.conf
 echo "}" >> /etc/danted.conf
 
-echo "[6] - Create a user"
+echo "[6] - Create a user, @ not allowed!"
 read -p "Enter user name: " user
-read -p "Enter user password: " pass
+read -p "Enter user password : " pass
 useradd -s /bin/false ${user}
 echo "$user:$pass" | chpasswd
 
@@ -59,21 +70,21 @@ systemctl restart danted
 systemctl enable danted
 
 echo "[8] - Checking proxy availability"
-for ((i = 7000; i <= 7010; i++))
+for ((i = ${port1}; i <= ${port2}; i++))
     do
       curl --socks5 ${user}:${pass}@$(ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet" | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1):${i} ident.me; echo
     done
 
 echo "[9] - For manual testing"
-for ((i = 7000; i <= 7010; i++))
+for ((i = ${port1}; i <= ${port2}; i++))
     do
       echo "curl --socks5 ${user}:${pass}@$(ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet" | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1):${i} ident.me; echo"
     done
 
 echo "[!] Your proxy list"
-for ((i = 7000; i <= 7010; i++))
+for ((i = ${port1}; i <= ${port2}; i++))
     do
       echo "${user}:${pass}@$(ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet" | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1):${i}"
     done
-
+fi
 echo "[10] - DONE!!!"
